8d862ef58946081ce13bf7d94e7087ddd3a062d6
kollier-martin <kollierm@yahoo.com>
2021-10-29 13:17:17 -0400

    Utils and Servlets push for early setup

---
+++ src/main/java/Util/JWTUtil.java
@@ -0,0 +1,45 @@
+package Util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+
+public class JWTUtil {
+    private static Key key;
+
+    private static void createKey(){
+        key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+    public static String createJWT(HttpServletRequest request){
+        createKey();
+
+        // Build the java web token
+        String jwt = Jwts.builder().setIssuer(request.getRequestURL().toString())
+                .setSubject(request.getParameter("username"))
+                .signWith(key)
+                .compact();
+
+        return jwt;
+    }
+
+    public static boolean parseJWT(String jwsString) {
+        boolean parsed = false;
+
+        try {
+            Jws<Claims> jws = Jwts.parserBuilder()  // Creates parser instance
+                    .setSigningKey(key)             // Specify the key to verify this jws signature
+                    .build()                        // Returns a new, thread-safe, parser
+                    .parseClaimsJws(jwsString);     // Parse the jws and return the original jws
+
+            parsed = true;
+        }catch (JwtException e){
+            // JWT is invalid
+            e.printStackTrace();
+        }
+
+        return parsed;
+    }
+}
