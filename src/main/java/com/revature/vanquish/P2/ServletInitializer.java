baad9368bd1538e7b6499f8ccbed42bedef12115
kollier-martin <kollierm@yahoo.com>
2021-10-29 10:42:40 -0400

    Spring Boot Initial Commit

---
+++ src/main/java/com/revature/vanquish/P2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.revature.vanquish.P2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(P2Application.class);
+	}
+
+}
