dce23855d05c356a25fddb2de55608f14fad8b03
andrewdpetersen <andrew.petersen@revature.net>
2021-11-01 18:41:21 -0500

    refactored models package into application package

--- src/main/java/Application/P2Application.java
+++ src/main/java/Application/P2Application.java
@@ -1,5 +1,6 @@
 package Application;
 
+import Application.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class P2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P2Application.class, args);
-	}
+		User andrew = new User();
+		andrew.setEmail("andrew@andrew.com");
+		andrew.setFirst_name("Andrew");
+		andrew.setLast_name("Petersen");
+		andrew.setUsername("andrewp");
+		andrew.setPassword("password1");
+		andrew.setRole(false);
+		andrew.setUser_id(1001);
 
+		System.out.println("My name is "+andrew.getFirst_name()+" and I just created an account!");
+	}
 }
--- src/main/java/Application/models/User.java
+++ src/main/java/Application/models/User.java
@@ -1,4 +1,4 @@
-package models;
+package Application.models;
 
 import org.springframework.stereotype.Component;
 
