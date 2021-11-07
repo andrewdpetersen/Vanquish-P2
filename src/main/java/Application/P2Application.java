150f390e77ac20e46bc7d480a07c1046ffc5f298
Erika-tech257 <erikajtech@gmail.com>
2021-11-07 17:04:06 -0500

    fixed merge issues

--- src/main/java/Application/P2Application.java
+++ src/main/java/Application/P2Application.java
@@ -1,16 +1,7 @@
 package Application;
-
-<<<<<<< HEAD
 import Application.models.Track;
 import Application.services.TrackService;
-=======
-import Application.models.Concert;
-import Application.models.Track;
-import Application.services.APIClientService;
 import Application.services.ConcertService;
-import Application.services.TrackService;
-import ch.qos.logback.core.net.SyslogOutputStream;
->>>>>>> bd93c80105452fa6d2fc22b344109e5ed39147a9
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -22,33 +13,26 @@ import javax.xml.bind.SchemaOutputResolver;
 
 
 @SpringBootApplication(scanBasePackages = "Application")
-<<<<<<< HEAD
 @EntityScan("Application.models")
 @ComponentScan("Application")
-=======
-@EntityScan("Application")
-@ComponentScan("Application")
 @EnableTransactionManagement
->>>>>>> bd93c80105452fa6d2fc22b344109e5ed39147a9
+
 public class P2Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(P2Application.class, args);
 
-<<<<<<< HEAD
+
 		Track bean = context.getBean(Track.class);
 		System.out.println(bean.getTitle());
 
-		TrackService service = context.getBean(TrackService.class);
+//		Track model = new Track(1, "test");
+//		Track model1 = new Track(2, "test2");
+//		Track model2 = new Track(3, "test3");
+//		service.save(model);
+//		service.save(model1);
+//		service.save(model2);
 
-		Track model = new Track(1, "test");
-		Track model1 = new Track(2, "test2");
-		Track model2 = new Track(3, "test3");
-		service.save(model);
-		service.save(model1);
-		service.save(model2);
-
-=======
 		TrackService service = context.getBean(TrackService.class);
 		ConcertService service2 = context.getBean(ConcertService.class);
 
@@ -65,6 +49,6 @@ public class P2Application {
 
 		Track track2 = service.getTrack(1);
 		System.out.println(track2.toString());
->>>>>>> bd93c80105452fa6d2fc22b344109e5ed39147a9
+
 	}
 }
