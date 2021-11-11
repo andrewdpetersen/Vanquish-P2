722e6c62bdf9883ca2dccb2d3a53827db256d210
Kollier Martin <kollierm@yahoo.com>
2021-11-11 17:51:47 -0500

    Update CORSFilter.java

--- src/main/java/Application/filters/CORSFilter.java
+++ src/main/java/Application/filters/CORSFilter.java
@@ -1,3 +1,14 @@
+package Application.filters;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Component
 public class CORSFilter extends OncePerRequestFilter {
 
