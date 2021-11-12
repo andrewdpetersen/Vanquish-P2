4395557bca2800076321e129fb648bf86202adf0
micoolio <mreece1999@gmail.com>
2021-11-12 15:40:16 -0600

    fix request mapping annotation on playlist controller

--- src/main/java/Application/controllers/PlaylistController.java
+++ src/main/java/Application/controllers/PlaylistController.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/4TheMusic", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/4TheMusic")
 public class PlaylistController {
     private final PlaylistService playlistService;
 
