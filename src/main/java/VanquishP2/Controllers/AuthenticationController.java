2b57620e8283c22df5eb2a1b035e8fb85e4eeb41
Kollier Martin <kollierm@yahoo.com>
2021-11-12 21:23:39 -0500

    AuthenticationController Added ResponseEntity returns and logic

--- src/main/java/VanquishP2/Controllers/AuthenticationController.java
+++ src/main/java/VanquishP2/Controllers/AuthenticationController.java
@@ -10,6 +10,10 @@ import VanquishP2.DTOs.UserRegistrationDTO;
 import jdk.nashorn.api.scripting.JSObject;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,16 +48,17 @@ public class AuthenticationController {
      * @return The new user and their key
      */
     @PostMapping(value = "/login", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public UserInfo authenticate(@RequestBody LoginCredentialsDTO credentials, HttpServletResponse response) {
+    public ResponseEntity<UserInfo> authenticate(@RequestBody LoginCredentialsDTO credentials, HttpServletResponse response) {
         System.out.println(credentials);
         Optional<UserInfo> userInfo = userInfoService.authenticate(credentials.getUsername(), credentials.getPassword());
 
         if (userInfo.isPresent()) {
             String jwt = jwtUtil.createJWT(userInfo.get());
             response.setHeader(jwtUtil.getHeader(), jwt);
+            return new ResponseEntity<>(userInfo.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
-        return userInfo.get();
     }
 
     /**
@@ -62,11 +67,16 @@ public class AuthenticationController {
      * @return The new registered User Data
      */
     @PostMapping(value = "/register/basic", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public User registerBasicUser(@RequestBody @Valid UserRegistrationDTO regData, HttpServletResponse response){
-        User user = userService.registerUser(regData, User.Role.PREMIUM);
+    public ResponseEntity<User> registerBasicUser(@RequestBody @Valid UserRegistrationDTO regData) {
+        User user = userService.registerUser(regData, User.Role.BASIC);
         String jwt = jwtUtil.createJWT(user.getUserInfo());
-        response.setHeader(jwtUtil.getHeader(), jwt);
-        return user;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add(jwtUtil.getHeader(), jwt);
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(user);
     }
 
     /**
@@ -75,10 +85,15 @@ public class AuthenticationController {
      * @return The new registered User Data
      */
     @PostMapping(value = "/register/premium", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public User registerPremiumUser(@RequestBody @Valid UserRegistrationDTO regData, HttpServletResponse response){
+    public ResponseEntity<User> registerPremiumUser(@RequestBody @Valid UserRegistrationDTO regData){
         User user = userService.registerUser(regData, User.Role.PREMIUM);
         String jwt = jwtUtil.createJWT(user.getUserInfo());
-        response.setHeader(jwtUtil.getHeader(), jwt);
-        return user;
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add(jwtUtil.getHeader(), jwt);
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(user);
     }
 }
