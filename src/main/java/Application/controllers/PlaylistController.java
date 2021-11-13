ef0551dec046df20399c7e98c34278e1b0337dab
andrewdpetersen <90648378+andrewdpetersen@users.noreply.github.com>
2021-11-13 09:00:17 -0600

    changed context path to include "playlist"
    changed context path to include "playlist"

--- src/main/java/Application/controllers/PlaylistController.java
+++ src/main/java/Application/controllers/PlaylistController.java
@@ -17,14 +17,14 @@ public class PlaylistController {
         this.playlistService = playlistService;
     }
 
-    @PostMapping(value = "/name", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/playlist", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Playlist savePlaylist_name(@RequestBody Playlist playlist){
         playlistService.savePlaylist(playlist);
         return playlistService.getPlaylist(playlist.getPlaylist_id());
     }
 
-    @GetMapping(value = "/name/{playlist_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/playlist/{playlist_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     public Playlist getPlaylistById(@PathVariable("playlist_id") Integer id){
         return playlistService.getPlaylist(id);
