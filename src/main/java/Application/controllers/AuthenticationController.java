67b32809de68fe1c9e32fdaa02f9f8ca1c79e92e
kollier-martin <kollierm@yahoo.com>
2021-11-17 17:10:09 -0500

    Controller Changes Removed All JWTUtil and few stray comments

--- src/main/java/Application/controllers/AuthenticationController.java
+++ src/main/java/Application/controllers/AuthenticationController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -31,11 +29,10 @@ public class AuthenticationController {
     /**
      * Authenticate and Login User
      * @param credentials username and password
-     * @param response The HTTP Response
      * @return The new user and their key
      */
     @PostMapping(value = "/login", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public UserInfo authenticate(@RequestBody LoginCredentialsDTO credentials, HttpServletResponse response) {
+    public UserInfo authenticate(@RequestBody LoginCredentialsDTO credentials) {
         Optional<UserInfo> userInfo = userInfoService.authenticate(credentials.getUsername(), credentials.getPassword());
 
         if (!userInfo.isPresent()){
@@ -57,7 +54,7 @@ public class AuthenticationController {
      * @return The new registered User Data
      */
     @PostMapping(value = "/register/basic", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public User registerBasicUser(@RequestBody @Valid UserRegistrationDTO regData, HttpServletResponse response){
+    public User registerBasicUser(@RequestBody @Valid UserRegistrationDTO regData){
         return userService.registerUser(regData, User.Role.BASIC);
     }
 
@@ -67,7 +64,7 @@ public class AuthenticationController {
      * @return The new registered User Data
      */
     @PostMapping(value = "/register/premium", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public User registerPremiumUser(@RequestBody @Valid UserRegistrationDTO regData, HttpServletResponse response){
+    public User registerPremiumUser(@RequestBody @Valid UserRegistrationDTO regData){
         return userService.registerUser(regData, User.Role.PREMIUM);
     }
 }
--- src/main/java/Application/controllers/LikeDislikeController.java
+++ src/main/java/Application/controllers/LikeDislikeController.java
@@ -1,17 +1,16 @@
 package Application.controllers;
 
 import Application.deezer.JSONStringToModelConverter;
+import Application.exceptions.UserDoesNotExistException;
 import Application.models.*;
 import Application.services.APIClientService;
 import Application.services.LikeDislikeService;
-import Application.utils.JWTUtil;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -26,11 +25,10 @@ import java.util.Optional;
 @RequestMapping(value = "/4TheMusic")
 public class LikeDislikeController {
     private final LikeDislikeService likeDislikeService;
-    private final JWTUtil jwtUtil;
+    private final String errMessage = "User with name %s does not exist!";
 
     @Autowired
-    public LikeDislikeController(JWTUtil jwtUtil, LikeDislikeService likeDislikeService) {
-        this.jwtUtil = jwtUtil;
+    public LikeDislikeController(LikeDislikeService likeDislikeService) {
         this.likeDislikeService = likeDislikeService;
     }
 
@@ -42,58 +40,29 @@ public class LikeDislikeController {
      */
     @PostMapping(value = "/like/{currentUser}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
-    public Track likeTrack(@RequestBody Track track,@PathVariable String currentUser,HttpServletRequest request) throws JSONException {
-        //get user from username
+    public Track likeTrack(@RequestBody Track track, @PathVariable String currentUser) throws JSONException {
         Optional<User> user = likeDislikeService.getUserByUsername(currentUser);
 
         if(user.isPresent())
         {
-            //if track exists on our database already, grab it from there to update
             Track database = likeDislikeService.getTrack(track.getTrack_id());
-            if(database != null)
-            {
-                // public Track(Integer track_id, String title, String duration, List<Playlist> playlists, List<User> userLikes, List<User> userDislikes, Artist artist, Album album)
+            if(database != null) {
                 Track databaseTrack = new Track(database.getTrack_id(),database.getTitle(),database.getDuration(),
                         database.getPlaylists(),database.getUserLikes(),database.getUserDislikes(),database.getArtist(),
                         database.getAlbum());
-                //add user to track's list of users who like it
+
                 databaseTrack.getUserLikes().add(user.get());
                 likeDislikeService.saveTrack(databaseTrack);
-            }
-            else //if it doesn't exist on our database already, we need to create it
-            {
-                //create new track with user on the list of users who like it
-                //create artist object to persist
-                Artist artist = new Artist(track.getArtist().getName(),track.getArtist().getID(),track.getArtist().getImage_url());
-                likeDislikeService.saveArtist(artist);
-                String albumRequest = "https://api.deezer.com/album/" + track.getAlbum().getID();
-                String stringJsonAlbum = APIClientService.get(albumRequest);
-                Album resultAlbum = JSONStringToModelConverter.albumConverter(stringJsonAlbum);
-                Genre genre = new Genre(resultAlbum.getGenre().getGenreID(),resultAlbum.getGenre().getGenreName(),resultAlbum.getGenre().getImageURL());
-                likeDislikeService.saveGenre(genre);
-                Album album = new Album(track.getAlbum().getID(),track.getAlbum().getAlbum_title(),track.getAlbum().getDate(), artist, genre);
-                likeDislikeService.saveAlbum(album);
-                Track newTrack = new Track(track.getTrack_id(),track.getTitle(),artist,album);
-                //System.out.println(newTrack.getTrack_id());
-                Album newAlbum = likeDislikeService.getAlbum(album.getID());
-                Artist newArtist = likeDislikeService.getArtist(artist.getID());
-                newArtist.getAlbums().add(album);
-                likeDislikeService.saveArtist(newArtist);
-                likeDislikeService.saveTrack(newTrack);
-                newAlbum.getTracks().add(newTrack);
-                likeDislikeService.saveAlbum(newAlbum);
-                newTrack.getUserLikes().add(user.get());
-                likeDislikeService.saveTrack(newTrack);
+            } else {
+                createNewArtist(track, user);
             }
 
-            //add track to user's liked list after either creating a new track or updating current one
-            //we know the track exists because we just persisted it.
             user.get().getLiked_tracks().add(likeDislikeService.getTrack(track.getTrack_id()));
             likeDislikeService.saveUser(user.get());
         }
         else
         {
-            //throw userdoesnotexistexception
+            throw new UserDoesNotExistException(String.format(errMessage, currentUser));
         }
 
         return likeDislikeService.getTrack(track.getTrack_id());
@@ -107,57 +76,31 @@ public class LikeDislikeController {
      */
     @PostMapping(value = "/dislike/{currentUser}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
-    public Track dislikeTrack(@RequestBody Track track,@PathVariable String currentUser,HttpServletRequest request) throws JSONException {
-        //get user from username
+    public Track dislikeTrack(@RequestBody Track track, @PathVariable String currentUser) throws JSONException {
         Optional<User> user = likeDislikeService.getUserByUsername(currentUser);
 
         if(user.isPresent())
         {
-            //if track exists on our database already, grab it from there to update
             Track database = likeDislikeService.getTrack(track.getTrack_id());
             if(database != null)
             {
-                // public Track(Integer track_id, String title, String duration, List<Playlist> playlists, List<User> userLikes, List<User> userDislikes, Artist artist, Album album)
                 Track databaseTrack = new Track(database.getTrack_id(),database.getTitle(),database.getDuration(),
                         database.getPlaylists(),database.getUserLikes(),database.getUserDislikes(),database.getArtist(),
                         database.getAlbum());
-                //add user to track's list of users who like it
+
                 databaseTrack.getUserDislikes().add(user.get());
                 likeDislikeService.saveTrack(databaseTrack);
             }
-            else //if it doesn't exist on our database already, we need to create it
-            {
-                //create new track with user on the list of users who like it
-                //create artist object to persist
-                Artist artist = new Artist(track.getArtist().getName(),track.getArtist().getID(),track.getArtist().getImage_url());
-                likeDislikeService.saveArtist(artist);
-                String albumRequest = "https://api.deezer.com/album/" + track.getAlbum().getID();
-                String stringJsonAlbum = APIClientService.get(albumRequest);
-                Album resultAlbum = JSONStringToModelConverter.albumConverter(stringJsonAlbum);
-                Genre genre = new Genre(resultAlbum.getGenre().getGenreID(),resultAlbum.getGenre().getGenreName(),resultAlbum.getGenre().getImageURL());
-                likeDislikeService.saveGenre(genre);
-                Album album = new Album(track.getAlbum().getID(),track.getAlbum().getAlbum_title(),track.getAlbum().getDate(), artist, genre);
-                likeDislikeService.saveAlbum(album);
-                Track newTrack = new Track(track.getTrack_id(),track.getTitle(),artist,album);
-                Album newAlbum = likeDislikeService.getAlbum(album.getID());
-                Artist newArtist = likeDislikeService.getArtist(artist.getID());
-                newArtist.getAlbums().add(album);
-                likeDislikeService.saveArtist(newArtist);
-                likeDislikeService.saveTrack(newTrack);
-                newAlbum.getTracks().add(newTrack);
-                likeDislikeService.saveAlbum(newAlbum);
-                newTrack.getUserDislikes().add(user.get());
-                likeDislikeService.saveTrack(newTrack);
+            else {
+                createNewArtist(track, user);
             }
 
-            //add track to user's disliked list after either creating a new track or updating current one
-            //we know the track exists because we just persisted it.
             user.get().getDisliked_tracks().add(likeDislikeService.getTrack(track.getTrack_id()));
             likeDislikeService.saveUser(user.get());
         }
         else
         {
-            //throw userdoesnotexistexception
+            throw new UserDoesNotExistException(String.format(errMessage, currentUser));
         }
 
         return likeDislikeService.getTrack(track.getTrack_id());
@@ -168,9 +111,43 @@ public class LikeDislikeController {
     public List<Integer> getRatioByTrackId(@PathVariable ("track_id") Integer track_id){
         Integer likes = likeDislikeService.getTotalLikes(track_id);
         Integer dislikes = likeDislikeService.getTotalDislikes(track_id);
+
         List<Integer> ratio = new LinkedList<>();
+
         ratio.add(likes);
         ratio.add(dislikes);
+
         return ratio;
+    }
+
+    private void createNewArtist(Track track, Optional<User> user) throws JSONException {
+        String albumRequest = "https://api.deezer.com/album/" + track.getAlbum().getID();
+        String stringJsonAlbum = APIClientService.get(albumRequest);
+
+        Artist artist = new Artist(track.getArtist().getName(), track.getArtist().getID(), track.getArtist().getImage_url());
+        likeDislikeService.saveArtist(artist);
+
+        Album resultAlbum = JSONStringToModelConverter.albumConverter(stringJsonAlbum);
+        Genre genre = new Genre(resultAlbum.getGenre().getGenreID(),resultAlbum.getGenre().getGenreName(),resultAlbum.getGenre().getImageURL());
+        likeDislikeService.saveGenre(genre);
+
+        Album album = new Album(track.getAlbum().getID(),track.getAlbum().getAlbum_title(),track.getAlbum().getDate(), artist, genre);
+        likeDislikeService.saveAlbum(album);
+
+        Track newTrack = new Track(track.getTrack_id(),track.getTitle(), artist,album);
+
+        Album newAlbum = likeDislikeService.getAlbum(album.getID());
+
+        Artist newArtist = likeDislikeService.getArtist(artist.getID());
+        newArtist.getAlbums().add(album);
+        likeDislikeService.saveArtist(newArtist);
+
+        likeDislikeService.saveTrack(newTrack);
+        newAlbum.getTracks().add(newTrack);
+
+        likeDislikeService.saveAlbum(newAlbum);
+        newTrack.getUserDislikes().add(user.get());
+
+        likeDislikeService.saveTrack(newTrack);
     }
 }
